778ec2b81ea3817d60bdf87c2b3b6c514062a668
Cherepanov Roman <romach007@gmail.com>
2015-05-08 21:20:19 +0300

    Add test

--- src/lesson_3/Calculate.java
+++ src/lesson_3/Calculate.java
@@ -1,13 +1,23 @@
 package lesson_3;
 
 public class Calculate {
-    public static void main(String[] args) {
-        System.out.println("Calculate...");
-        double first = Double.valueOf(args[0]);
-        double second = Double.valueOf(args[2]);
-        double result = 0.0;
+    public static void main(String[] args) throws Exception {
+	if (args.length == 0) {
+            test();
+        } else {
+            System.out.println("Calculate...");	
+	
+            double result = calculate(args);       
+        
+            System.out.println("Result : " + result);
+        }
+    }
 
-        switch (args[1]) {
+    private static double calculate(String[] args) throws Exception {
+	double first = Double.valueOf(args[0]);
+        double second = Double.valueOf(args[2]);
+        double result = 0.0;        
+	switch (args[1]) {
             case "+" :
                 result = first + second;
                 break;
@@ -27,9 +37,25 @@ public class Calculate {
                 }
                 break;
             default:
-                System.out.println("Wrong operator!");
-                return;
+                throw new Exception("Wrong operator");
         }
-        System.out.println("Result : " + result);
+	return result;
+    }
+
+    private static void test() throws Exception {
+        String[][] testCases = {{"3", "+", "10", "13"},
+                                {"5", "-", "7", "-2"},
+                                {"3", "/", "2", "1.5"},
+                                {"4", "*", "5", "20"},
+                                {"5", "^", "2", "25"},
+                               };
+	
+        for (int i = 0; i < testCases.length; i++) {
+            double result = calculate(new String[]{testCases[i][0], testCases[i][1], testCases[i][2]});
+            double expectedResult = Double.valueOf(testCases[i][3]);
+            String testCaseOutput = "TestCase #" + (i + 1) + " result: " + 
+                                    (Double.compare(result, expectedResult) == 0 ? "OK": "Error");
+            System.out.println(testCaseOutput);
+        } 
     }
 }
